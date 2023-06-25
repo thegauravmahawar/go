e79b78c0544e79e79c2f499990f0eed9169a00cf
Gaurav Mahawar <thegauravmahawar@gmail.com>
2023-06-25 17:46:46 +0530

    Constants in Go

---
+++ gobyexample/04-constants.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+// const declares a constant value.
+const s string = "constant"
+
+func main() {
+
+    fmt.Println(s)
+    
+    const n = 500000000
+
+    // Constant expressions perform arithmetic with arbitrary precision.
+    const d = 3e20 / n
+    fmt.Println(d)
+
+
+    // A numeric constant has no type until it's given one.
+    fmt.Println(int64(d))
+
+    fmt.Println(math.Sin(n))
+
+}
